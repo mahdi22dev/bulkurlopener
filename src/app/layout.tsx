35621fd4df2103c3f12039f6ccc3ebbbe51eaab8
mahdi22dev <idrissimahdi2020@gmail.com>
2024-07-05 13:57:26 +0100

    ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "./_components/Nav";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
