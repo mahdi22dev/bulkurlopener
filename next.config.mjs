04c4474c860c1fc8f5c1e934fbccdafc1f78c069
mahdi22dev <idrissimahdi2020@gmail.com>
2024-07-30 13:04:08 +0100

    d

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/", // The path you want to redirect from
-        destination: "https://content-locked.com/?63eb359", // The external domain or URL you want to redirect to
-        permanent: true, // Set to `false` for temporary redirects
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
